6d4b01b7e05a876299416a53094543604457b701
JavadHPo <72570629+JavadHPo@users.noreply.github.com>
2020-12-14 20:22:25 -0700

    plot file is added

--- Hosseinpour/pemfc_polarization.py
+++ Hosseinpour/pemfc_polarization.py
@@ -6,10 +6,10 @@ i_array = np.linspace(0.00000001,100000,50)
 V_cell = np.zeros_like(i_array)
 
 for j, current in enumerate(i_array):
-    print(current)
+    #print(current)
     solution = pemfc_model(current)
     V_cell[j] = solution.y[-1,-1] - solution.y[0,-1]
-    print(V_cell[j])
+    #print(V_cell[j])
 
 
 from matplotlib import pyplot as plt
